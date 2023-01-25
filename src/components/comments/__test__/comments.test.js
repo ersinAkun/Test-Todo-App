b9f9253931e154eb4f70f54853fc626f84458ecf
Ersin Akun <ersinakun34@gmail.com>
2023-01-25 22:27:22 +0300

    v5

--- src/components/comments/__test__/comments.test.js
+++ src/components/comments/__test__/comments.test.js
@@ -1,12 +1,24 @@
+import { render, screen } from "@testing-library/react";
 import axios from "axios";
+import { Await } from "react-router-dom";
+import Comments from "../comments";
 
 jest.mock("axios");
 
 describe("coments", () => {
 
+    const comments = [
+        {id: 1, body:"John"},
+        {id: 2, body:"Michael"}
+    ];
 
-
-
+    
+    it("should render the first card", async() => {
+        axios.get.mockResolvedValueOnce( {data: comments});
+        render(<Comments/>);
+        const commentEl = await screen.findByTestId("comment-card-1")
+        expect(commentEl).toBeInTheDocument();
+    });
 
 })
 
