0552a955f1a62378a151b6cf640bdaf2c20494d0
Ersin Akun <ersinakun34@gmail.com>
2023-01-25 22:42:37 +0300

    v6

--- src/components/comments/__test__/comments.test.js
+++ src/components/comments/__test__/comments.test.js
@@ -1,29 +1,34 @@
 import { render, screen } from "@testing-library/react";
 import axios from "axios";
-import { Await } from "react-router-dom";
 import Comments from "../comments";
 
 jest.mock("axios");
+describe("comments", () => {
 
-describe("coments", () => {
+  const comments = [
+    { id: 1, body: "John" },
+    { id: 2, body: "Michael" },
+    { id: 3, body: "Joseph" },
+  ];
 
-    const comments = [
-        {id: 1, body:"John"},
-        {id: 2, body:"Michael"}
-    ];
+  beforeEach( ()=>{
+    axios.get.mockResolvedValueOnce({ data: comments });
+    render(<Comments />);
+  })
 
-    
-    it("should render the first card", async() => {
-        axios.get.mockResolvedValueOnce( {data: comments});
-        render(<Comments/>);
-        const commentEl = await screen.findByTestId("comment-card-1")
-        expect(commentEl).toBeInTheDocument();
-    });
+  it("should render the first card", async () => {
+    const commentEl = await screen.findByTestId("comment-card-1");
+    expect(commentEl).toBeInTheDocument();
+  });
 
-})
+  it("should render 3 card", async () => {
+    const commentEls = await screen.findAllByTestId(/comment-card-/i);
+    expect(commentEls.length).toBe(3);
+  });
 
-
-
-
-
-
+  it("should render comment body", async ()=>{
+    const commentEl = await screen.findByTestId("comment-body-1");
+    expect(commentEl.innerHTML).toBe("John");
+  })
+  
+});
