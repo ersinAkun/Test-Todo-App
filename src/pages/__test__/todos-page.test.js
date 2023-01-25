827e29c74af6f13aef8948a5778be27ef27a4a1e
Ersin Akun <ersinakun34@gmail.com>
2023-01-25 21:23:56 +0300

    v1

--- src/pages/__test__/todos-page.test.js
+++ src/pages/__test__/todos-page.test.js
@@ -2,18 +2,30 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import TodosPage from "../todos-page"
 
-
+const addTask = (task) => { 
+        const buttonEl = screen.getByRole("button", {name:"Add"});
+        const inputEl = screen.getByPlaceholderText(/Type some text/i);
+        userEvent.type(inputEl, task);
+        userEvent.click(buttonEl);
+ }
 
 describe( "adding functionality", ()=>{
 
     it("should render new task in the list", ()=>{
         render(<TodosPage/>);
-        const buttonEl = screen.getByRole("button", {name:"Add"});
-        const inputEl = screen.getByPlaceholderText(/Type some text/i);
-        userEvent.type(inputEl, "hello react");
-        userEvent.click(buttonEl);
-
-        const listItemEl = screen.getByText(/hello react/i);
+        addTask("This task is for testing purpose")        
+        const listItemEl = screen.getByText(/This task is for testing purpose/i);
         expect(listItemEl).toBeInTheDocument();
+    })
+})
+
+
+
+describe ("completed functionality", ()=>{
+    it("task should not have 'completed' class when it is initially created", ()=>{
+        render (<TodosPage/>);
+        addTask("Hi there!");
+        const listItemEl = screen.getByText(/Hi there!/i);
+        expect(listItemEl).not.toHaveClass("completed");
     })
 })
